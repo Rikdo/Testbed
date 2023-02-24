3b0f5698059688b165612a761807dec751e9e302
Rikdo <Dash.McL@gmail.com>
2023-02-24 11:13:35 -0800

    Updated Triangle answer

--- SQL/HackerRankAnswers.sql
+++ SQL/HackerRankAnswers.sql
@@ -93,13 +93,11 @@ from station
 --Triangles
 SELECT 
     CASE
-        WHEN A + B <= C OR A + C <= B OR B + C <= A
-            THEN 'Not A Triangle'
-        WHEN A = B AND A = C 
-            THEN 'Equilateral'
-        WHEN (A = B AND B != C) OR (A != B AND B = C)
-            THEN 'Isosceles'
-        WHEN A != B AND A != C AND B != C
-            THEN 'Scalene'
-    END AS 'Output'
+        WHEN (A + B <= C) OR (A + C <= B) OR (B + C <= A) THEN 'Not A Triangle'
+        ELSE CASE
+            WHEN A = B AND A = C THEN 'Equilateral'
+            WHEN (A = B AND A != C) OR (A = C AND A != B) OR (B = C AND B != A) THEN 'Isosceles'
+            ELSE 'Scalene'
+        END
+    END AS "Output"
 FROM TRIANGLES;
